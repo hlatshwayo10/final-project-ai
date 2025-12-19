37ee09c3a9ee1018b22fcc9db7542778d2a78945
= <=>
2025-12-19 03:54:09 -0500

    error handling server function

--- server.py
+++ server.py
@@ -7,7 +7,18 @@ app = Flask("Emotion Detector")
 @app.route("/emotionDetector")
 def emotionDetector():
     text_to_analyze = request.args.get('textToAnalyze')
+    if not text_to_analyze or text_to_analyze.strip() == "":
+        empty_response = {
+            "anger": None,
+            "disgust": None,
+            "fear": None,
+            "joy": None,
+            "sadness": None,
+            "dominant_emotion": None
+        }
+        return "Invalid Text! Please try again"
     response = emotion_detector(text_to_analyze)
+
     return response
 
 @app.route("/")
@@ -15,4 +26,4 @@ def render_index_page():
     return render_template('index.html')
 
 if __name__ == "__main__":
-     app.run(host="0.0.0.0", port=5000)
+     app.run(host="0.0.0.0", port=5001)
